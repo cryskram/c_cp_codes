ee3deea9515a98cf9c36890d530c85bc35246fdf
Vageesh <65950037+cryskram@users.noreply.github.com>
2025-06-27 11:47:11 +0530

    Update tsp.c

--- exam/tsp.c
+++ exam/tsp.c
@@ -3,45 +3,34 @@
 
 #define N 4
 
-int minCost = INT_MAX;
-int path[N];
-int visited[N];
-
 int cost[N][N] = {
     {0, 12, 10, 19},
     {12, 0, 3, 7},
     {10, 3, 0, 6},
-    {19, 7, 6, 0}};
+    {19, 7, 6, 0}
+};
 
-void tsp(int currentCity, int count, int currentCost)
-{
-    if (count == N && cost[currentCity][0])
-    {
-        int totalCost = currentCost + cost[currentCity][0];
-        if (totalCost < minCost)
-            minCost = totalCost;
+int visited[N], minCost = INT_MAX;
+
+void tsp(int pos, int count, int costSoFar) {
+    if (count == N) {
+        if (costSoFar + cost[pos][0] < minCost)
+            minCost = costSoFar + cost[pos][0];
         return;
     }
 
-    for (int i = 0; i < N; i++)
-    {
-        if (!visited[i] && cost[currentCity][i])
-        {
+    for (int i = 0; i < N; i++) {
+        if (!visited[i]) {
             visited[i] = 1;
-            tsp(i, count + 1, currentCost + cost[currentCity][i]);
+            tsp(i, count + 1, costSoFar + cost[pos][i]);
             visited[i] = 0;
         }
     }
 }
 
-int main()
-{
-    for (int i = 0; i < N; i++)
-        visited[i] = 0;
-
+int main() {
     visited[0] = 1;
     tsp(0, 1, 0);
-
-    printf("Minimum cost for TSP (using Branch and Bound): %d\n", minCost);
+    printf("Minimum cost: %d\n", minCost);
     return 0;
 }
